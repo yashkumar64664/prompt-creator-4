fb4528169ee9614a8ff810c2f563ab842da73ca5
yashkumar64664 <yashkumar64664@gmail.com>
2026-01-15 18:33:01 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+app.listen(5000, () => {
+  console.log("Backend running on port 5000");
+});
